cbc81c96b645af1e80aae63d6696d91be5c0769f
HelloWorld <karol.tobolewski@gmail.com>
2024-12-29 15:06:23 +0100

    test

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {                           // [1]
+    public static void main(String[] args) {         // [2]
+        System.out.println("Hello World!"); // [3]
+        System.out.println("Karol");
+        System.out.println("20");
+        System.out.println("T");
+    }
+}
