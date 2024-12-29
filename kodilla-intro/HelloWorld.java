b2a2dc5f61c4870a65fa3e8037bf9f51eb493bee
HelloWorld <karol.tobolewski@gmail.com>
2024-12-29 15:51:47 +0100

    test2

---
+++ kodilla-intro/HelloWorld.java
@@ -0,0 +1,5 @@
+public class HelloWorld {                           // [1]
+    public static void main(String[] args) {         // [2]
+        System.out.println("Hello World!");          // [3]
+    }
+}
---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String myName = "Karol";
+        int myAge = 27;
+        double weight = 79.1;
+        boolean isGoodGamer = false;
+        String country = "Polska";
+        int steamLevel = 52;
+        System.out.println(myName);
+        System.out.println(myAge);
+        System.out.println(weight);
+        System.out.println(isGoodGamer);
+        System.out.println(country);
+        System.out.println(steamLevel);
+    }
+}
