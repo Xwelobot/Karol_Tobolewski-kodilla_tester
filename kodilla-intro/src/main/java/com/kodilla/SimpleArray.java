e2dca4e9f4de17fc63a72d06af4accc183b72ef7
HelloWorld <karol.tobolewski@gmail.com>
2025-01-06 16:03:50 +0100

    test3

--- kodilla-intro/src/main/java/com/kodilla/SimpleArray.java
+++ kodilla-intro/src/main/java/com/kodilla/SimpleArray.java
@@ -2,13 +2,17 @@ package com.kodilla;
 
 public class SimpleArray {
     public static void main(String[] args) {
-        String[] names = new String[5];
-        names[0] = "Karol";
-        names[1] = "Kacper";
-        names[2] = "Kamil";
-        names[3] = "Michal";
-        names[4] = "Mikolaj";
-        String name = names[2];
-        System.out.println(names[2]);
+        String[] movies = new String[5];
+        movies[0] = "Harry Potter";
+        movies[1] = "Lord of the Rings";
+        movies[2] = "Forrest Gump";
+        movies[3] = "Heretic";
+        movies[4] = "Hooligans";
+
+        String movie = movies[4];
+        System.out.println(movie);
+
+        int numberOfElements = movies.length;
+        System.out.println(numberOfElements);
     }
 }
