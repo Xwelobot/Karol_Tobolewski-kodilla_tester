0bebf7ebd88e0f3ae0e16c86b8b3f41f4e272713
HelloWorld <karol.tobolewski@gmail.com>
2025-01-13 14:18:56 +0100

    zadanie: refaktoryzacja kodu

---
+++ kodilla-intro/src/main/java/com/kodilla/Application.java
@@ -0,0 +1,29 @@
+package com.kodilla;
+
+public class User {
+    String name;
+    double age;
+    double height;
+
+    public User(String name, double age, double height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+    public void Application() {
+        if (name != null) {
+            if (age > 30 && height > 160) {
+                System.out.println("User is older than 30 and taller than 160cm");
+            } else {
+                System.out.println("User is 30 (or younger) or 160cm (or shorter)");
+            }
+        }
+    }
+}
+public static Application {
+    public static void main(String[] args) {
+        User user = new User("Adam", 40.5, 178);
+        user.Application();
+    }
+}
