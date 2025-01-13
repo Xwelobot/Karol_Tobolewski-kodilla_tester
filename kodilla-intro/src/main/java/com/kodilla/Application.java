1fafdcb6e32106f558306cdbc2bb9dd0d2478012
HelloWorld <karol.tobolewski@gmail.com>
2025-01-13 14:20:06 +0100

    zadanie: petle

--- kodilla-intro/src/main/java/com/kodilla/Application.java
+++ kodilla-intro/src/main/java/com/kodilla/Application.java
@@ -21,7 +21,7 @@ public class User {
         }
     }
 }
-public static Application {
+public class Application {
     public static void main(String[] args) {
         User user = new User("Adam", 40.5, 178);
         user.Application();
