243a631eb957fab66bcadb0ef382c3b123a8258f
HelloWorld <karol.tobolewski@gmail.com>
2025-01-14 18:17:39 +0100

    zadanie: dziedziczenie w praktyce

--- kodilla-basic-tests/src/main/java/com/kodilla/inheritance/homework/RunSystem.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/inheritance/homework/RunSystem.java
@@ -4,11 +4,11 @@ public class RunSystem {
     public static void main(String[] args){
 
         Windows windows = new Windows(1997, 0);
-        System.out.println(windows.getWin());
+        System.out.println("Year of release was " + windows.getWin());
         windows.turnOn();
 
         MacIo macio = new MacIo(0, 1991);
-        System.out.println(macio.getMac());
+        System.out.println("Year of release was " + macio.getMac());
         MacIo.turnOff();
 
     }
