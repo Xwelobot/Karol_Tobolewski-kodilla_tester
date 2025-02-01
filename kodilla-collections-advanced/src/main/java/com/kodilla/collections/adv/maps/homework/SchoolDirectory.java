71aca60a6bc9e6d86545c166427c0528ffc4cb1c
PC <karol.tobolewski@gmail.com>
2025-02-01 16:20:43 +0100

    poprawka

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/maps/homework/SchoolDirectory.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/maps/homework/SchoolDirectory.java
@@ -2,7 +2,6 @@ package com.kodilla.collections.adv.maps.homework;
 
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class SchoolDirectory {
