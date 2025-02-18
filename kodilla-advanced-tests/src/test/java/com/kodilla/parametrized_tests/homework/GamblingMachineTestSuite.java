58515fe3919c927e33ba013195e9033b45ca1756
PC <karol.tobolewski@gmail.com>
2025-02-18 17:22:05 +0100

    Zadanie: maszyna do gier, testy

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTestSuite.java
@@ -2,42 +2,29 @@ package com.kodilla.parametrized_tests.homework;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GamblingMachineTestSuite {
-    private Set<Integer> numbers(String numbersStr) {
-        return Set.of(numbersStr.split(","))
-                .stream()
-                .map(Integer::parseInt)
-                .collect(Collectors.toSet());
-    }
 
     private GamblingMachine gamblingMachine = new GamblingMachine();
 
     @ParameterizedTest
-    @CsvFileSource(resources ="/canThereBeMoreNumbersThanSix.csv", numLinesToSkip = 1)
-    public void canThereBeMoreNumbersThanSix(int number){
-        assertFalse(gamblingMachine.equals(number));
-    }
+    @CsvFileSource(resources = "/canNumberInScopeApply.csv")
+    public void canNumberInScopeApply(String input) throws InvalidNumbersException {
+        Set<Integer> numbers = Arrays.stream(input.split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toSet());
 
-    @ParameterizedTest
-    @CsvFileSource(resources = "/isAnyNumberOutOfDeclaredScope.csv", numLinesToSkip = 1)
-    public void canNumberBeOutOfScope(int number){
-        assertFalse(gamblingMachine.equals(number));
-    }
-
-    @ParameterizedTest
-    @CsvFileSource(resources = "/canNumberInScopeApply.csv", numLinesToSkip = 1)
-    public void canNumberInScopeApply(String userNumbersStr, int expectedWins) throws InvalidNumbersException {
-
-        Set<Integer> number = numbers(userNumbersStr);
-        int wins = gamblingMachine.howManyWins(number);
-        assertEquals(expectedWins, wins);
+//        Set<Integer> path = numbers;
+        System.out.println(numbers);
+//        assertTrue(gamblingMachine.howManyWins(path)>=0);
 
     }
 }
