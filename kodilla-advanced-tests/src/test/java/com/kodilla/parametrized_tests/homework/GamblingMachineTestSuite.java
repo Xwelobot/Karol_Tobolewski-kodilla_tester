d0559ffad401d5dd607e99ebe8b650ebbc1340bc
PC <karol.tobolewski@gmail.com>
2025-02-18 21:10:50 +0100

    Zadanie: maszyna do gier

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTestSuite.java
@@ -1,5 +1,6 @@
 package com.kodilla.parametrized_tests.homework;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
@@ -15,16 +16,40 @@ class GamblingMachineTestSuite {
 
     private GamblingMachine gamblingMachine = new GamblingMachine();
 
+
     @ParameterizedTest
     @CsvFileSource(resources = "/canNumberInScopeApply.csv")
     public void canNumberInScopeApply(String input) throws InvalidNumbersException {
-        Set<Integer> numbers = Arrays.stream(input.split(","))
+        Set<Integer> numbers = Arrays.stream(input.split(";"))
                 .map(Integer::parseInt)
                 .collect(Collectors.toSet());
 
-//        Set<Integer> path = numbers;
+        System.out.println(input);
         System.out.println(numbers);
-//        assertTrue(gamblingMachine.howManyWins(path)>=0);
+        assertTrue(gamblingMachine.howManyWins(numbers)>=0);
 
+    }
+    @ParameterizedTest
+    @CsvFileSource(resources = "/canBeFewerNumbers.csv")
+    public void canBeFewerNumbers(String input) throws InvalidNumbersException {
+        Set<Integer> numbers = Arrays.stream(input.split(";"))
+                .map(Integer::parseInt)
+                .collect(Collectors.toSet());
+
+        assertThrows(InvalidNumbersException.class, () -> {
+            new GamblingMachine().howManyWins(numbers);
+        });
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/canBeNumberOutOfScope.csv")
+    public void canBeNumberOutOfScope(String input) throws InvalidNumbersException {
+        Set<Integer> numbers = Arrays.stream(input.split(";"))
+                .map(Integer::parseInt)
+                .collect(Collectors.toSet());
+
+        assertThrows(InvalidNumbersException.class, () -> {
+            new GamblingMachine().howManyWins(numbers);
+        });
     }
 }
