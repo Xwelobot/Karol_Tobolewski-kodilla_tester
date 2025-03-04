9a8f0979d50153bcf21049587aced374c968ae14
PC <karol.tobolewski@gmail.com>
2025-03-04 20:49:14 +0100

    poprawki

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/SimpleApplication.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/SimpleApplication.java
@@ -1,12 +1,14 @@
 package com.kodilla.spring.basic.spring_dependency_injection;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SimpleApplication {
 
-    private SkypeMessageService messageService;
+    @Qualifier(value = "SkypeMessageService")
+    private MessageService messageService;
 
     public String processMessage(String message, String receiver) {
         if (checkReceiver(receiver)) {
