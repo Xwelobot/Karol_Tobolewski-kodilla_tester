d78420b7d8e2a23a2f3d526d97230f4fe65fd5d3
Xwelobot <karol.tobolewski@gmail.com>
2025-03-31 15:19:18 +0200

    Zadanie: Ćwiczymy SoapUI

---
+++ kodilla-selenium/src/main/java/com/kodilla/soap/TestsSoap.java
@@ -0,0 +1,36 @@
+//package com.kodilla.soap;
+//
+//public class TestsSoap {
+//<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:soap="http://kodilla.com/courses/soap">
+//   <soapenv:Header/>
+//   <soapenv:Body>
+//      <soap:getCourseRequest>
+//         <soap:name>Java Developer</soap:name>
+//      </soap:getCourseRequest>
+//   </soapenv:Body>
+//</soapenv:Envelope>
+//
+//    Asseration do contains technology>JAVA<
+//
+//<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:soap="http://kodilla.com/courses/soap">
+//   <soapenv:Header/>
+//   <soapenv:Body>
+//      <soap:getCourseRequest>
+//         <soap:name>Java Developer Plus</soap:name>
+//      </soap:getCourseRequest>
+//   </soapenv:Body>
+//</soapenv:Envelope>
+//
+//    Asseration do contains technology>JAVA<
+//
+//<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:soap="http://kodilla.com/courses/soap">
+//   <soapenv:Header/>
+//   <soapenv:Body>
+//      <soap:getCourseRequest>
+//         <soap:name>Python Developer</soap:name>
+//      </soap:getCourseRequest>
+//   </soapenv:Body>
+//</soapenv:Envelope>
+//
+//    Asseration do not contains technology>PYTHON<
+//}
