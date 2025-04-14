a1f31d81674ff5817eebc045d274c02920498050
Xwelobot <karol.tobolewski@gmail.com>
2025-04-14 15:55:45 +0200

    Zadanie: praca z JaCoCo i SonarLint

--- kodilla-code-coverage/src/test/java/com/kodilla/jacoco/YearTestSuite.java
+++ kodilla-code-coverage/src/test/java/com/kodilla/jacoco/YearTestSuite.java
@@ -7,10 +7,48 @@ import static org.junit.jupiter.api.Assertions.*;
 class YearTestSuite {
 
     @Test
-    void isLeap() {
+    void isLeapTrue() {
 
         //given
         Year year = new Year(1600);
+
+        //When
+        boolean leap = year.isLeap();
+
+        //Then
+        assertTrue(leap);
+    }
+    @Test
+    void isLeapFalse() {
+
+        //given
+        Year year = new Year(1630);
+
+        //When
+        boolean leap = year.isLeap();
+
+        //Then
+        assertFalse(leap);
+    }
+
+    @Test
+    void isLeapFalseAgain() {
+
+        //given
+        Year year = new Year(1000);
+
+        //When
+        boolean leap = year.isLeap();
+
+        //Then
+        assertFalse(leap);
+    }
+
+    @Test
+    void isLeapFalseAgainAgain() {
+
+        //given
+        Year year = new Year(1004);
 
         //When
         boolean leap = year.isLeap();
